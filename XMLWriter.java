47256e35713a95f3c2b2dfb5f64655874bb5aa49
Maharshi Panchal <panchalmaharshi8@gmail.com>
2023-10-31 13:53:34 -0700

    fixed errors

--- XMLWriter.java
+++ XMLWriter.java
@@ -1,6 +1,10 @@
 
-
 import javax.xml.parsers.*;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.*;
 import java.io.*;
 
